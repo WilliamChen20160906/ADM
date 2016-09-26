3a328543fcd3b0f40d6663fe3ffefe10bdb6b91c
william chen <william_chen@leadwell.com.tw>
2016-09-26 10:45:48 +0800

    A test project from WilliamChen 2016/09/26 10:45 am

---
+++ EIPWebApplicationWilliam1/EIPWebApplicationWilliam1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EIPWebApplicationWilliam1.Global" Language="C#" %>
---
+++ EIPWebApplicationWilliam1/EIPWebApplicationWilliam1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EIPWebApplicationWilliam1.Startup))]
+namespace EIPWebApplicationWilliam1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
